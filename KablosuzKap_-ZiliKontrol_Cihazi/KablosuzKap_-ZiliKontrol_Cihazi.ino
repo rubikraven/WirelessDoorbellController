cac852efbf9d17d3727c08006e28de8e1e1e4d8d
Cihan Engin <71848220+rubikraven@users.noreply.github.com>
2023-08-29 00:21:02 +0300

    Add files via upload

---
+++ KablosuzKap_-ZiliKontrol_Cihazi/KablosuzKap_-ZiliKontrol_Cihazi.ino
@@ -0,0 +1,30 @@
+#include <RCSwitch.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+// I2C adresinizi buraya girin. Genellikle 0x27'dir, fakat bazen 0x3F olabilir.
+LiquidCrystal_I2C lcd(0x27, 16, 2);  
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.backlight();
+  mySwitch.enableTransmit(10);  // Verici modülünün bağlı olduğu pin. Bu örnekte 10
+  mySwitch.setProtocol(5);  // Protokol numarasını 5 olarak belirler.
+  mySwitch.setPulseLength(740); // PulseLength'i 740 olarak belirler.
+}
+
+void loop() {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Gonderilen kod: ");
+  lcd.setCursor(0,1);
+  lcd.print(123456);  // Gönderilen kodu LCD'ye yazdır.
+  
+  // Sinyali bir kere gönderme
+  mySwitch.send(123456, 24);  // İlk parametre gönderilecek veri, ikinci parametre veri boyutu
+  delay(100);  // Yüz milisaniye bekleyin
+  
+  delay(1000);  // 1 saniye bekleyin
+}
